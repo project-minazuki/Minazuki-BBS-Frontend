122531ba20263ee34f134e7671a7f43f416e0e1f
NagiCarol <44332726+NagiCarol@users.noreply.github.com>
2020-07-04 02:16:27 +0800

    会写的写完了

--- src/pages/PostPage.tsx
+++ src/pages/PostPage.tsx
@@ -1,27 +1,30 @@
 import React, {FC, memo} from "react";
 
 import {PostPageProps} from "../containers/pages/PostPage";
-import '../styles/Categorypage.scss';
-import {Announcement, Category, Post, User} from "../configs/types";
+import '../styles/PostPage.scss';
+import {Announcement, Category, Post, User, Theme} from "../configs/types";
 import PostTools from "../components/PostTools";
 import CateCard from "../components/CateCard";
-import NewReply from "../components/NewReply";
 import atri from "../images/bg-atri.png";
 import BgImg from "../components/BgImg";
 import Reply from "../components/Reply";
+import ThemeContentCard from "../components/ThemeContentCard";
 
 
 const PostPage: FC<PostPageProps> = memo(({}) => {
 
 
     return (
-        <div id='page-Post' className='container'>
+        <div id='page-Post' className='page container'>
             <BgImg src={atri}/>
             <div className='left'>
+                <ThemeContentCard editor={{} as User} theme={{} as Theme}/>
                 <Reply post={{} as Post} user={{} as User}/>
             </div>
-            <PostTools/>
-            <CateCard info={{} as Category}/>
+            <div className='right'>
+                <PostTools/>
+                <CateCard info={{} as Category}/>
+            </div>
         </div>
 
     )
