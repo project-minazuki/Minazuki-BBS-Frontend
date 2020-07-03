55e4c0ea64a4ebea979967a4ca61b0eedb7e8b1d
NagiCarol <44332726+NagiCarol@users.noreply.github.com>
2020-07-04 01:18:46 +0800

    手动合代码中

--- src/pages/PostPage.tsx
+++ src/pages/PostPage.tsx
@@ -2,12 +2,13 @@ import React, {FC, memo} from "react";
 
 import {PostPageProps} from "../containers/pages/PostPage";
 import '../styles/Categorypage.scss';
-import {Announcement, Category, User} from "../configs/types";
+import {Announcement, Category, Post, User} from "../configs/types";
 import PostTools from "../components/PostTools";
 import CateCard from "../components/CateCard";
 import NewReply from "../components/NewReply";
 import atri from "../images/bg-atri.png";
 import BgImg from "../components/BgImg";
+import Reply from "../components/Reply";
 
 
 const PostPage: FC<PostPageProps> = memo(({}) => {
@@ -15,12 +16,12 @@ const PostPage: FC<PostPageProps> = memo(({}) => {
 
     return (
         <div id='page-Post' className='container'>
-            <BgImg src={atri} />
+            <BgImg src={atri}/>
             <div className='left'>
-
+                <Reply post={{} as Post} user={{} as User}/>
             </div>
             <PostTools/>
-            <CateCard info={{}as Category}/>
+            <CateCard info={{} as Category}/>
         </div>
 
     )
