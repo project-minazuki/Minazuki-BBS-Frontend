e066212d452b5e3d9be6077f8d99989f2881e506
七海 椎名 <ymd45921@163.com>
2020-07-04 10:45:34 +0800

    fix: wyy 的抽屉
    - useState 失效，原因未知

--- src/components/PostTools.tsx
+++ src/components/PostTools.tsx
@@ -1,11 +1,8 @@
 import React, {CSSProperties, FC, HTMLProps, memo, useState} from "react";
-import '../styles/components/ThemeItem.scss';
 
-import {message, Pagination, Radio, Drawer, Button} from 'antd';
+import {message, Pagination, Radio, Drawer, Button, Form, Input} from 'antd';
 import {useHistory} from "react-router";
 import '../styles/components/PostTools.scss'
-import returnIcon from "../images/returnIcon.png";
-import tipOff from "../images/tip-off.png";
 
 import {
     WarningOutlined, CommentOutlined
@@ -14,23 +11,33 @@ import ThemeItem from "./ThemeItem";
 import {Category, Theme, User} from "../configs/types";
 
 interface IProps {
-
 }
 
 
 const PostTools: FC<IProps> = memo((props) => {
 
-    const [visible, setVisible] = useState(false);//todo：无作用
+    const [open, setOpen] = useState({on: false});//todo：无作用
     const history = useHistory();
+    const [form] = Form.useForm();
+    const formValues = {
+
+    }
+
+    const handleSubmit = () => {
+        // todo
+    }
+
     const backToHome = () => history.push('/')
     const handleClickTipOff = () => {
         message.info('你正在尝试举报')
     }
-    const handleClicketurn = () => {
-        setVisible(true);
+    const handleReply = () => {
+        setOpen({on: true});
     }
     const onClose = () => {
-        setVisible(false);
+        const old = open;
+        setOpen({on: false});
+        console.log('onClose', old, open)
     }
 
     //TODO:事件监听
@@ -53,25 +60,50 @@ const PostTools: FC<IProps> = memo((props) => {
                 <div className='tip-off-word'>举报</div>
 
             </div>
-            <div className='return' onClick={handleClicketurn}>
+            <div className='return' onClick={handleReply}>
                 <CommentOutlined/>
                 <div className='return-word'>回复</div>
                 <Drawer width={480}
                     className='Drawer'
                     placement="right"
-                    closable={false}
+                    closable={true}
                     onClose={onClose}
-                    visible={visible}//todo:这个visible不起作用
+                    forceRender
+                    visible={open.on}//todo:这个visible不起作用
                 >
-                    <div className='new-reply-post-word'>新建回复贴</div>
-                    <div className='new-will-reply-post-word'>您要回复的主题帖</div>
-                    <div className='new-reply-post'>
-                        <ThemeItem
-                            info_Theme={{} as Theme}info_Cate={{}as Category} info_User={{}as User}
-                            className='new-will-reply-post'>
-                        </ThemeItem>
+                    {/*<div className='new-reply-post-word'>新建回复贴</div>*/}
+                    {/*<div className='new-will-reply-post-word'>您要回复的主题帖</div>*/}
+                    {/*<div className='new-reply-post'>*/}
+                    {/*    <ThemeItem*/}
+                    {/*        info_Theme={{} as Theme}info_Cate={{}as Category} info_User={{}as User}*/}
+                    {/*        className='new-will-reply-post'>*/}
+                    {/*    </ThemeItem>*/}
+                    {/*</div>*/}
+                    {/*<div className='new-will-reply-post-word'>您要回复的内容</div>*/}
+                    <div className='drawer-container'>
+                        <div className='drawer-title'>
+                            新建回复贴
+                        </div>
+                        <div className='desc-content'>
+                            请在下面的文本框中输入你要进行回复的内容
+                        </div>
+                        <Form form={form} initialValues={formValues} onFinish={handleSubmit}
+                              className='new-form'>
+                            <Form.Item name='content'>
+                                <Input.TextArea />
+                            </Form.Item>
+                            <Form.Item className='buttons'>
+                                <Button shape='round' onClick={() => setOpen({on: false})}
+                                        className='button' size='large'>
+                                    取消
+                                </Button>
+                                <Button shape='round' htmlType='submit' type='primary'
+                                        className='button' size='large'>
+                                    提交
+                                </Button>
+                            </Form.Item>
+                        </Form>
                     </div>
-                    <div className='new-will-reply-post-word'>您要回复的内容</div>
                 </Drawer>
             </div>
         </div>
